4711e188961a40ead4076f42472425a3390ee364
Théo Delas <theo.delas@gmail.com>
2022-03-31 16:59:55 +0200

    Create Enterprise entity, dto, controller, service, module

---
+++ src/enterprise/entities/enterprise.entity.ts
@@ -0,0 +1,33 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Enterprise {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  // @TODO : 9 numbers
+  @Column({ unique: true })
+  siren: number;
+
+  // @TODO : 280 char max
+  @Column()
+  description: string;
+
+  @Column()
+  logoUrl: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
