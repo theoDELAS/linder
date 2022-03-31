efad251e6b30ce7b2f0517faa22b00bc182847f3
Saluc00 <l.usereau@outlook.fr>
2022-03-31 17:23:14 +0200

    Add Entity Offer

--- src/offer/entities/offer.entity.ts
+++ src/offer/entities/offer.entity.ts
@@ -1,1 +1,35 @@
-export class Offer {}
+import {
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Column,
+} from 'typeorm';
+
+export class Offer {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column({ length: 280 })
+  description: string;
+
+  @Column('array')
+  keyWord: Array<string>;
+
+  @Column('int')
+  salary: number;
+
+  @Column('text')
+  type: string;
+
+  @Column('text')
+  status: string;
+
+  @Column('boolean')
+  isForRecruter: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
