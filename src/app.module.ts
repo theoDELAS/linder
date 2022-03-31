51eef1c7073128196ba81c6e370ccd65de4ab5b8
Théo Delas <theo.delas@gmail.com>
2022-03-31 16:58:28 +0200

    Setup postgres and TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Connection } from 'typeorm';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Enterprise } from './enterprise/entities/enterprise.entity';
+import { EnterpriseController } from './enterprise/enterprise.controller';
+import { EnterpriseService } from './enterprise/enterprise.service';
+import { EnterpriseModule } from './enterprise/enterprise.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: '',
+      password: '',
+      database: 'linder',
+      entities: [Enterprise],
+      synchronize: true,
+    }),
+    EnterpriseModule,
+  ],
+  controllers: [AppController, EnterpriseController],
+  providers: [AppService, EnterpriseService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
